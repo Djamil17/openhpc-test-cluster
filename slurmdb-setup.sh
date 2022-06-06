9c8d713d977319477dd8cc898fc1896aa8591cd2
Djamil17 <dl2774@columbia.edu>
2022-06-06 13:01:38 -0400

    fix(open-hpc-cluster): fix the sacctmgr error with users
    Remove sacctmgr users and simply use test account

--- slurmdb-setup.sh
+++ slurmdb-setup.sh
@@ -6,7 +6,5 @@ sacctmgr -i add account test
 sacctmgr -i add account research
 sacctmgr -i add account proj1 parent=research
 sacctmgr -i add account proj2 parent=research
-sacctmgr -i add user djamil account=proj1
-sacctmgr -i add user rodgers account=proj2
-sacctmgr -i add user matthew account=proj2
+sacctmgr -i add user test account=proj1
 systemctl start slurmctld
