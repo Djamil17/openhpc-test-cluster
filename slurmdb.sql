346f352d93a876d61f8df8532416b03f5eac64ba
Djamil17 <dl2774@columbia.edu>
2022-06-06 13:03:15 -0400

    fix(open-hpc-cluster): fix the slurmdb error with hostname
    changed hostname in creation of slurm databse, in slurmdbd.sql, to sms from localhost

--- slurmdb.sql
+++ slurmdb.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE slurm_accounting;
-CREATE USER 'slurm'@'localhost' IDENTIFIED BY 'abc123slurmdbd';
-GRANT ALL PRIVILEGES ON slurm_accounting.* TO 'slurm'@'localhost';
+CREATE USER 'slurm'@'sms' IDENTIFIED BY 'abc123slurmdbd';
+GRANT ALL PRIVILEGES ON slurm_accounting.* TO 'slurm'@'sms';
 
 # CREATE DATABASE slurm_accounting;
 # CREATE USER 'slurm'@'ace-dev-ctrl' IDENTIFIED BY 'abc123slurmdbd';
